6ba155ab67db8c855133aa6f268c2d691d109613
Carl-Olivier N'Diaye <48607882+Amnesycks@users.noreply.github.com>
2020-07-03 14:06:13 +0000

    Update ping.js
    Correction of ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("**You can not use this command!**")
-
     let début = Date.now();
 
     await message.channel.send("Ping").then(async(m) => await m.edit(`Pong: ${Date.now() - début} ms`));
